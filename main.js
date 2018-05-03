26fab0278f027d240c87bb60e209ef9726c93920
Mathew Basnett <lofar968@gmail.com>
2018-05-02 17:38:01 -0700

    Checking for dups and display hand functions

--- main.js
+++ main.js
@@ -2,30 +2,57 @@ var deck = [  1  ,   1 ,   1  ,   1  ,  2  ,   2 ,   2  ,   2  ,   3  ,  3  ,  3
               6  ,   6  ,   6  ,   6  ,   7  ,    7  ,   7  ,   7  , 8  ,    8  ,    8  ,    8  ,    9  ,    9  ,   9  ,   9  ,   10   ,   10  ,  10  , 10  ,
               11  , 11  , 11  , 11  , 12  ,  12  , 12  , 12  , 13  ,  13  , 13  , 13  ];
 
-var player1Hand = [];
-var player2Hand = [];
+var playerHand = [];
+var aiHand = [];
 
-function GetRandomInt(min, max) {
+var score = 0;
+
+
+function GetRandomInt(min, max)
+{
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function DealCards(){
-  for(var i = 0; i < 5; i++){
+function DealCards()
+{
+  for(var i = 0; i < 5; i++)
+  {
     var x = GetRandomInt(0, 51);
-    player1Hand.push(deck[x]);
-
-      }
-  for(var i = 0; i < 5; i++){
-    var x = GetRandomInt(0, 51);
-    player2Hand.push(deck[x]);
-
-          }
-    console.log(player1Hand.length);
-    console.log(player2Hand.length);
+    playerHand.push(deck[x]);
   }
+  for(var i = 0; i < 5; i++)
+  {
+    var x = GetRandomInt(0, 51);
+    aiHand.push(deck[x]);
+  }
+  DisplayHand(playerHand);
+}
 
-function GoFish(){
-  if(i = 0; i < )
+//Display the hand
+function DisplayHand(hand)
+{
+  console.log(hand);
+}
+
+//Computer reads playerHand and checks for "books" (cards of the same face value)
+/*function CheckForDuplicates(hand)
+{
+  var results = [];
+  var sortedHand = hand.slice().sort();
+
+  for (var i = 0; i < sortedHand.length - 1; i++) {
+      if (sortedHand[i + 1] == sortedHand[i]) {
+        results.push(sortedHand[i]);
+      }
+  //return results;
+  console.log(results);
+  }
+} */
+
+function GoFish ()
+{
+  console.log("Ask the Ai if they have a card you have")
+  if
 }
